86fbbb8610092cf38a69ff1973ba7ae197651366
Hyun Cheol <ironlove77@gmail.com>
2015-03-23 21:03:13 +0900

    멀티미디어 예제 추가

--- src/com/example/myexample/MultiMedia/MultiMedia_04_Activity.java
+++ src/com/example/myexample/MultiMedia/MultiMedia_04_Activity.java
@@ -2,7 +2,9 @@ package com.example.myexample.MultiMedia;
 
 import android.app.Activity;
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,33 +15,76 @@ import java.io.IOException;
 
 public class MultiMedia_04_Activity extends Activity implements View.OnClickListener {
 
-    MediaPlayer music;
+    private MediaPlayer mPlayer;
+    private MediaRecorder mRecorder;
+    private String path = Environment.getExternalStorageDirectory().getPath() + "/audio_test.mp3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_activity_08);
+        setContentView(layout.activity_multimedia_04);
 
-        Button btnStart = (Button) findViewById(R.id.btnStart);
-        Button btnStop = (Button) findViewById(R.id.btnStop);
-        btnStart.setOnClickListener(this);
-        btnStop.setOnClickListener(this);
+        Button btnStartRecording = (Button) findViewById(R.id.btnStartRecording);
+        Button btnStopRecording = (Button) findViewById(R.id.btnStopRecording);
+        Button btnStartPlay = (Button) findViewById(R.id.btnStartPlay);
+        Button btnStopPlay = (Button) findViewById(R.id.btnStopPlay);
 
-        music = MediaPlayer.create(this, R.raw.konan);
+        btnStartRecording.setOnClickListener(this);
+        btnStopRecording.setOnClickListener(this);
+        btnStartPlay.setOnClickListener(this);
+        btnStopPlay.setOnClickListener(this);
+
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        mRecorder.setOutputFile(path);
+
     }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnStart:
-                music.start();
+            case R.id.btnStartRecording:
+                try {
+                    // 레코더 준비
+                    mRecorder.prepare();
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // 녹음 시작
+                mRecorder.start();
 
                 break;
-            case R.id.btnStop:
-                music.stop();
+            case R.id.btnStopRecording:
                 try {
-                    music.prepare();
+                    // 녹음 정지
+                    mRecorder.stop();
+                    // 다시 사용할 수 있도록 레코더 상태를 리셋
+                    mRecorder.reset();
+                } catch (IllegalStateException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            case R.id.btnStartPlay:
+                mPlayer = new MediaPlayer();
+                try {
+                    mPlayer.setDataSource(path);
+                    mPlayer.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mPlayer.start();
+
+                break;
+            case R.id.btnStopPlay:
+                mPlayer.stop();
+                try {
+                    mPlayer.prepare();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
--- src/com/example/myexample/MultiMedia/MultiMedia_06_Activity.java
+++ src/com/example/myexample/MultiMedia/MultiMedia_06_Activity.java
@@ -2,49 +2,65 @@ package com.example.myexample.MultiMedia;
 
 import android.app.Activity;
 import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.example.myexample.R;
-import com.example.myexample.R.layout;
 
 import java.io.IOException;
 
 public class MultiMedia_06_Activity extends Activity implements View.OnClickListener {
-
-    MediaPlayer music;
+    SurfaceHolder holder;
+    private MediaPlayer mPlayer;
+    private String path = Environment.getExternalStorageDirectory().getPath() + "/video_test.mp3";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_activity_08);
 
-        Button btnStart = (Button) findViewById(R.id.btnStart);
-        Button btnStop = (Button) findViewById(R.id.btnStop);
-        btnStart.setOnClickListener(this);
-        btnStop.setOnClickListener(this);
+        setContentView(R.layout.activity_multimedia_06);
 
-        music = MediaPlayer.create(this, R.raw.konan);
+        SurfaceView surface = new SurfaceView(this);
+        holder = surface.getHolder();
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        FrameLayout frame = (FrameLayout) findViewById(R.id.videoLayout);
+        frame.addView(surface);
+
+        Button btnStartPlay = (Button) findViewById(R.id.btnStartPlay);
+        Button btnStopPlay = (Button) findViewById(R.id.btnStopPlay);
+
+        btnStartPlay.setOnClickListener(this);
+        btnStopPlay.setOnClickListener(this);
+
     }
-
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnStart:
-                music.start();
+            case R.id.btnStartPlay:
+                mPlayer = new MediaPlayer();
 
-                break;
-            case R.id.btnStop:
-                music.stop();
+
                 try {
-                    music.prepare();
-                } catch (IOException e) {
+                    mPlayer.setDataSource(path);
+                    mPlayer.setDisplay(holder);
+
+                    mPlayer.prepare();
+                    mPlayer.start();
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
                 break;
-            default:
+            case R.id.btnStopPlay:
+                mPlayer.stop();
+                mPlayer.release();
                 break;
         }
     }
