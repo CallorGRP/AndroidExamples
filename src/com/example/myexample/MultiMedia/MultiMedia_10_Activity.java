7a6bb798041c8ea7674b7fe58ddcd2856794f0ce
Hyun Cheol <ironlove77@gmail.com>
2015-03-30 23:51:28 +0900

    네트워크 예제 추가

--- src/com/example/myexample/MultiMedia/MultiMedia_10_Activity.java
+++ src/com/example/myexample/MultiMedia/MultiMedia_10_Activity.java
@@ -1,51 +1,96 @@
 package com.example.myexample.MultiMedia;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.myexample.R;
 
+public class MultiMedia_10_Activity extends Activity {
 
-public class MultiMedia_10_Activity extends Activity implements View.OnClickListener {
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private Camera camera;
+    private ImageView imageview;
+    private boolean inProgress;
 
-    private static final int REQ_CAMERA = 100;
-    private ImageView image_camera;
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQ_CAMERA) {
-            if (resultCode == Activity.RESULT_OK) {
-                Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-                image_camera.setImageBitmap(bitmap);
-            }
-        }
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_multimedia_08);
 
-        Button btnCamera = (Button) findViewById(R.id.btnCamera);
-        image_camera = (ImageView)findViewById(R.id.image_camera);
-        btnCamera.setOnClickListener(this);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
+        setContentView(R.layout.activity_multimedia_10);
+
+        imageview = (ImageView) findViewById(R.id.imageView1);
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView1);
+
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(surfaceListener);
+
+        findViewById(R.id.btnTakePicture).setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if (camera != null && inProgress == false) {
+                            camera.takePicture(null, null, takePicture);
+                            inProgress = true;
+                        }
+                    }
+                }
+        );
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnCamera:
-                Intent intent = new Intent();
-                intent.setAction("android.media.action.IMAGE_CAPTURE");
-                startActivityForResult(intent, REQ_CAMERA);
 
-                break;
+    private Camera.PictureCallback takePicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            imageview.setImageBitmap(bitmap);
+            camera.startPreview();
+            inProgress = false;
+
+
         }
-    }
+    };
+
+    private SurfaceHolder.Callback surfaceListener = new SurfaceHolder.Callback() {
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            camera.release();
+            camera = null;
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            camera = Camera.open();
+
+            try {
+                camera.setPreviewDisplay(holder);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.setPreviewSize(width, height);
+            camera.startPreview();
+
+        }
+    };
 }
+
--- src/com/example/myexample/Network/Network_06_Activity.java
+++ src/com/example/myexample/Network/Network_06_Activity.java
@@ -1,51 +1,146 @@
 package com.example.myexample.Network;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.myexample.R;
-import com.example.myexample.R.layout;
 
-import java.io.IOException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Vector;
 
 public class Network_06_Activity extends Activity implements View.OnClickListener {
-
-    MediaPlayer music;
+    // http://whois.kisa.or.kr/openapi/whois.jsp?query=naver.co.kr&key=2015033021490816419014&answer=json
+    String whois_url = "http://whois.kisa.or.kr/openapi/whois.jsp?key=2015033021490816419014&answer=json&";
+    JSONTask JSONTask = new JSONTask();
+    EditText input;
+    Button button;
+    TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_multimedia_01);
+        setContentView(R.layout.activity_network_06);
+        input = (EditText) findViewById(R.id.input);
+        button = (Button) findViewById(R.id.button);
+        output = (TextView) findViewById(R.id.output);
+        button.setOnClickListener(this);
 
-        Button btnStart = (Button) findViewById(R.id.btnStart);
-        Button btnStop = (Button) findViewById(R.id.btnStop);
-        btnStart.setOnClickListener(this);
-        btnStop.setOnClickListener(this);
-
-        music = MediaPlayer.create(this, R.raw.konan);
     }
 
+    public class JSONTask extends AsyncTask<String, Void, String> {
+
+        Vector<String> vTitle = new Vector<>();
+        Vector<String> vDesc = new Vector<>();
+        URL url;
+        String tag = "";
+        String title = "";
+        String desc = "";
+
+        @Override
+        protected String doInBackground(String... params) {
+            InputStream is = null;
+            char[] buffer = new char[1000];
+
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+
+                conn.connect();
+                int response = conn.getResponseCode();
+                is = conn.getInputStream();
+
+
+                Reader reader = null;
+                reader = new InputStreamReader(is, "UTF-8");
+                reader.read(buffer);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return new String(buffer);
+        }
+
+        @Override
+        protected void onPostExecute(String strings) {
+            super.onPostExecute(strings);
+            try {
+                JSONObject json = new JSONObject(strings);
+                JSONObject whois = json.getJSONObject("whois");
+                JSONObject krdomain = whois.getJSONObject("krdomain");
+                String name = krdomain.getString("name");
+                String regName = krdomain.getString("regName");
+                String addr = krdomain.getString("addr");
+                String post = krdomain.getString("post");
+                String adminName = krdomain.getString("adminName");
+                String adminEmail = krdomain.getString("adminEmail");
+                String adminPhone = krdomain.getString("adminPhone");
+                String regDate = krdomain.getString("regDate");
+                String endDate = krdomain.getString("endDate");
+                String infoYN = krdomain.getString("infoYN");
+                String agency = krdomain.getString("agency");
+                String agency_url = krdomain.getString("agency_url");
+                String e_regName = krdomain.getString("e_regName");
+                String e_addr = krdomain.getString("e_addr");
+                String e_adminName = krdomain.getString("e_adminName");
+                String e_agency = krdomain.getString("e_agency");
+                String dnssec = krdomain.getString("dnssec");
+                String ns1 = krdomain.getString("ns1");
+                String ns2 = krdomain.getString("ns2");
+                String ns3 = krdomain.getString("ns3");
+
+                StringBuilder sb = new StringBuilder();
+                sb.append("name:").append(name).append("\n");
+                sb.append("regName:").append(regName).append("\n");
+                sb.append("addr:").append(addr).append("\n");
+                sb.append("post:").append(post).append("\n");
+                sb.append("adminName:").append(adminName).append("\n");
+                sb.append("adminEmail:").append(adminEmail).append("\n");
+                sb.append("adminPhone:").append(adminPhone).append("\n");
+                sb.append("regDate:").append(regDate).append("\n");
+                sb.append("endDate:").append(endDate).append("\n");
+                sb.append("infoYN:").append(infoYN).append("\n");
+                sb.append("agency:").append(agency).append("\n");
+                sb.append("agency_url:").append(agency_url).append("\n");
+                sb.append("e_regName:").append(e_regName).append("\n");
+                sb.append("e_addr:").append(e_addr).append("\n");
+                sb.append("e_adminName:").append(e_adminName).append("\n");
+                sb.append("e_agency:").append(e_agency).append("\n");
+                sb.append("dnssec:").append(dnssec).append("\n");
+                sb.append("ns1:").append(ns1).append("\n");
+                sb.append("ns2:").append(ns2).append("\n");
+                sb.append("ns3:").append(ns3).append("\n");
+
+                output.setText(sb);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnStart:
-                music.start();
+            case R.id.button:
+                String data = input.getText().toString();
+                JSONTask.execute(whois_url +"&query="+data, null, null);
 
-                break;
-            case R.id.btnStop:
-                music.stop();
-                try {
-                    music.prepare();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                break;
-            default:
-                break;
         }
     }
+
 }
