ccbb3df6b2643ca9ba7ce1a1da652353b21ace9f
Hyun Cheol <ironlove77@gmail.com>
2015-04-03 01:27:45 +0900

    네트워크 예제 추가

--- src/com/example/myexample/Network/Network_08_Activity.java
+++ src/com/example/myexample/Network/Network_08_Activity.java
@@ -1,48 +1,48 @@
 package com.example.myexample.Network;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import com.example.myexample.R;
 import com.example.myexample.R.layout;
 
-import java.io.IOException;
-
 public class Network_08_Activity extends Activity implements View.OnClickListener {
 
-    MediaPlayer music;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_multimedia_01);
+        setContentView(layout.activity_network_08);
 
-        Button btnStart = (Button) findViewById(R.id.btnStart);
-        Button btnStop = (Button) findViewById(R.id.btnStop);
-        btnStart.setOnClickListener(this);
-        btnStop.setOnClickListener(this);
+        webView = (WebView) findViewById(R.id.webView);
+        Button btnWeb = (Button) findViewById(R.id.btnWeb);
+        Button btnAsset = (Button) findViewById(R.id.btnAsset);
 
-        music = MediaPlayer.create(this, R.raw.konan);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+
+        btnWeb.setOnClickListener(this);
+        btnAsset.setOnClickListener(this);
+
     }
 
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btnStart:
-                music.start();
-
+            case R.id.btnWeb:
+                webView.loadUrl("http://www.google.com/");
                 break;
-            case R.id.btnStop:
-                music.stop();
-                try {
-                    music.prepare();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            case R.id.btnAsset:
+                webView.loadUrl("file:///android_asset/index.html");
+
                 break;
             default:
                 break;
