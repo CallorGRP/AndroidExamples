1cc4b81f39ab9169fd6779a03c1ade5ba62ee2ed
Hyun Cheol <ironlove77@gmail.com>
2015-04-13 23:01:02 +0900

    위치정보 예제 추가

---
+++ src/Location/Location_01_Activity.java
@@ -0,0 +1,45 @@
+package Location;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+import com.example.myexample.R;
+import com.example.myexample.R.layout;
+
+public class Location_01_Activity extends Activity implements View.OnClickListener {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_webview_01);
+
+        webView = (WebView) findViewById(R.id.webView);
+        Button btnWeb = (Button) findViewById(R.id.btnWeb);
+        Button btnAsset = (Button) findViewById(R.id.btnAsset);
+
+        btnWeb.setOnClickListener(this);
+        btnAsset.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnWeb:
+                webView.loadUrl("http://www.google.com/");
+                break;
+            case R.id.btnAsset:
+                webView.loadUrl("file:///android_asset/index.html");
+
+                break;
+            default:
+                break;
+        }
+    }
+}
