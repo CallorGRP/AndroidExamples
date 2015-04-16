b4cb7fbaad188a1d21e9b932916d890aec2dfe29
Hyun Cheol <ironlove77@gmail.com>
2015-04-17 00:48:20 +0900

    위치정보 예제 추가

--- src/com/example/myexample/Location/Location_02_Activity.java
+++ src/com/example/myexample/Location/Location_02_Activity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -37,54 +38,61 @@ public class Location_02_Activity extends Activity implements OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnStart:
-                manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 0, myListener);
+                manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, myListener);
                 break;
             case R.id.btnStop:
                 manager.removeUpdates(myListener);
                 break;
             case R.id.btnGetting:
-                Location location = manager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
                 if (location != null) {
-                    String message = String.format(
-                            "현재 위치의 위도와 경도 : ",
-                            location.getLongitude(), location.getLatitude()
-                    );
+                    String message = String.format("현재 위치의 위도와 경도 : ", location.getLongitude(), location.getLatitude());
                     Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
                 }
-
-
                 break;
 
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        manager.removeUpdates(myListener);
     }
 
     private class MyListener implements LocationListener {
         public void onLocationChanged(Location location) {
             Double latitude = location.getLatitude();
             Double longitude = location.getLongitude();
-            String message = String.format(
-                    "현재 위치의 위도와 경도 : ",
-                    location.getLongitude(), location.getLatitude()
-            );
+            String message = String.format("현재 위치의 위도와 경도 : %f,%f", latitude, longitude);
             Toast.makeText(Location_02_Activity.this, message, Toast.LENGTH_SHORT).show();
 
         }
 
         public void onProviderDisabled(String provider) {
-            Toast.makeText(Location_02_Activity.this,
-                    "위치서비스가 종료되었습니다.",Toast.LENGTH_LONG).show();
-
+            Toast.makeText(Location_02_Activity.this, "위치서비스가 종료되었습니다.", Toast.LENGTH_LONG).show();
         }
 
         public void onProviderEnabled(String provider) {
-            Toast.makeText(Location_02_Activity.this,
-                    "위치서비스가 시작되었습니다.",Toast.LENGTH_LONG).show();
-
+            Toast.makeText(Location_02_Activity.this, "위치서비스가 시작되었습니다.", Toast.LENGTH_LONG).show();
         }
 
         public void onStatusChanged(String provider, int status, Bundle extras) {
+            String message = "";
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    message = provider + ": 서비스가 시작되었습니다.";
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    message = provider + ": 서비스가 불가능합니다.";
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    message = provider + ": 서비스가 일시적으로 불가능합니다.";
+                    break;
+            }
+            Toast.makeText(Location_02_Activity.this,message,Toast.LENGTH_SHORT).show();
         }
     }
-}
 
+}
