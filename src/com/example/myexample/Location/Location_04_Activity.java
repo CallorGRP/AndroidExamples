7ab22dd70497741f2cf13f45479907b504e762e9
Hyun Cheol <ironlove77@gmail.com>
2015-05-09 12:14:01 +0900

    위치정보 예제 추가

---
+++ src/com/example/myexample/Location/Location_04_Activity.java
@@ -0,0 +1,17 @@
+package com.example.myexample.Location;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.myexample.R;
+
+public class Location_04_Activity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_04);
+
+    }
+
+}
