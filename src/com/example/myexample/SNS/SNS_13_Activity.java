b246fa1edb22e5eada7fcd60ad2d0253e245063a
Hyun Cheol <ironlove77@gmail.com>
2015-07-27 23:36:21 +0900

    SNS 예제 추가 ( 트위터 )

--- src/com/example/myexample/SNS/SNS_13_Activity.java
+++ src/com/example/myexample/SNS/SNS_13_Activity.java
@@ -7,13 +7,21 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.myexample.R;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.tweetcomposer.TweetComposer;
+
+import io.fabric.sdk.android.Fabric;
 
 public class SNS_13_Activity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TwitterAuthConfig authConfig =  new TwitterAuthConfig("vjaoe0JxKau4jFEX4jHVK8BWp", "92wRFWQWHEasfZwT4co7qVADMzftz2wGrbtiGwymAbeZ3raXfV");
+        Fabric.with(this, new TwitterCore(authConfig), new TweetComposer());
+
         setContentView(R.layout.activity_sns_13);
         Button btn_tweet = (Button) findViewById(R.id.btn_tweet);
         btn_tweet.setOnClickListener(this);
