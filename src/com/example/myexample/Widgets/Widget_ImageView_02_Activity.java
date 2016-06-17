f2176923657631eb226cf3c862b6f262a8d99342
Hyun Cheol <ironlove77@gmail.com>
2016-06-18 01:17:09 +0900

    이미지뷰(ImageView) scaleType 예제수정

--- src/com/example/myexample/Widgets/Widget_ImageView_02_Activity.java
+++ src/com/example/myexample/Widgets/Widget_ImageView_02_Activity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.myexample.R;
+import com.google.android.gms.vision.text.Text;
 
 public class Widget_ImageView_02_Activity extends Activity implements View.OnClickListener {
 
     ImageView ivImage;
+    TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,9 @@ public class Widget_ImageView_02_Activity extends Activity implements View.OnCli
         Button btnFitStart = (Button) findViewById(R.id.btnFitStart);
         Button btnFitEnd = (Button) findViewById(R.id.btnFitEnd);
         Button btnFitXY = (Button) findViewById(R.id.btnFitXY);
-        ivImage = (ImageView) findViewById(R.id.iv);
+
+        tvText = (TextView) findViewById(R.id.tvText);
+        ivImage = (ImageView) findViewById(R.id.ivImage);
 
         btnCenter.setOnClickListener(this);
         btnCenterCrop.setOnClickListener(this);
@@ -40,24 +45,31 @@ public class Widget_ImageView_02_Activity extends Activity implements View.OnCli
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnCenter:
+                tvText.setText("CENTER");
                 ivImage.setScaleType(ImageView.ScaleType.CENTER);
                 break;
             case R.id.btnCenterCrop:
+                tvText.setText("CENTER_CROP");
                 ivImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
                 break;
             case R.id.btnCenterInside:
+                tvText.setText("CENTER_INSIDE");
                 ivImage.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
                 break;
             case R.id.btnFitCenter:
+                tvText.setText("FIT_CENTER");
                 ivImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
                 break;
             case R.id.btnFitStart:
+                tvText.setText("FIT_START");
                 ivImage.setScaleType(ImageView.ScaleType.FIT_START);
                 break;
             case R.id.btnFitEnd:
+                tvText.setText("FIT_END");
                 ivImage.setScaleType(ImageView.ScaleType.FIT_END);
                 break;
             case R.id.btnFitXY:
+                tvText.setText("FIT_XY");
                 ivImage.setScaleType(ImageView.ScaleType.FIT_XY);
                 break;
         }
