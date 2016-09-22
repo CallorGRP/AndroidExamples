cf37ef2940b018d7c1bfe69686f4cf15eb5786a9
ironlove77@gmail.com <ironlove77@gmail.com>
2016-09-22 21:57:32 +0900

    그리드뷰 예제 추가

--- src/io/android_tech/myexample/AdapterView/AdapterView_GridView_01_Activity.java
+++ src/io/android_tech/myexample/AdapterView/AdapterView_GridView_01_Activity.java
@@ -1,88 +1,85 @@
 package io.android_tech.myexample.AdapterView;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import io.android_tech.myexample.R;
 
-public class AdapterView_GridView_01_Activity extends Activity {
+public class AdapterView_GridView_01_Activity extends Activity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    private static final int viewList[] = {R.id.custom_list_title_main, R.id.custom_list_title_sub, R.id.custom_list_image};
+
+    private static final String[] CONTACT_PROJECTION = new String[]{ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.CONTACT_ID};
+    AdapterView_ListView_CursorAdapter_02_Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_adapterview_gridview_01);
 
-        AdapterView_GridView_01_Adapter adapter = new AdapterView_GridView_01_Adapter(
-                this, R.layout.activity_adapterview_gridview_01_row,
-                getContactList());
-
-        GridView list = (GridView) findViewById(R.id.gridView1);
-        list.setAdapter(adapter);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+        } else {
+            startQuery();
+        }
     }
 
-    private ArrayList<AdapterView_GridView_01_Data> getContactList() {
+    private void startQuery() {
+        mAdapter = new AdapterView_ListView_CursorAdapter_02_Adapter(this,
+                R.layout.activity_adapterview_gridview_01_row, null,
+                new String[]{ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.CONTACT_ID},
+                viewList, 0);
+        GridView gridView = (GridView) findViewById(io.android_tech.myexample.R.id.gridView1);
+        gridView.setAdapter(mAdapter);
 
-        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        LoaderManager.LoaderCallbacks<Cursor> loaderCallbacks = this;
+        LoaderManager loaderManager = getLoaderManager();
+        loaderManager.initLoader(0, null, loaderCallbacks);
+    }
 
-        String[] projection = new String[]{
-
-                ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
-                ContactsContract.CommonDataKinds.Phone.NUMBER,
-                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
-        String[] selectionArgs = null;
-
-        String sortOrder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
-                + " COLLATE LOCALIZED ASC";
-
-        Cursor contactCursor = managedQuery(uri, projection, null,
-                selectionArgs, sortOrder);
-
-        ArrayList<AdapterView_GridView_01_Data> contactlist = new ArrayList<AdapterView_GridView_01_Data>();
-
-        if (contactCursor.moveToFirst()) {
-
-            do {
-
-                String phonenumber = contactCursor.getString(1).replaceAll("-",
-
-                        "");
-
-                if (phonenumber.length() == 10) {
-
-                    phonenumber = phonenumber.substring(0, 3) + "-"
-
-                            + phonenumber.substring(3, 6) + "-"
-
-                            + phonenumber.substring(6);
-
-                } else if (phonenumber.length() > 8) {
-
-                    phonenumber = phonenumber.substring(0, 3) + "-"
-
-                            + phonenumber.substring(3, 7) + "-"
-
-                            + phonenumber.substring(7);
-
-                }
-
-                AdapterView_GridView_01_Data acontact = new AdapterView_GridView_01_Data();
-                acontact.Image_ID = (int) contactCursor.getLong(0);
-                acontact.Sub_Title = phonenumber;
-                acontact.Main_Title = contactCursor.getString(2);
-
-                contactlist.add(acontact);
-
-            } while (contactCursor.moveToNext());
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startQuery();
+            } else {
+                Toast.makeText(this, "연락처에 대한 권한이 없어 실행할수 없습니다.", Toast.LENGTH_SHORT).show();
+            }
         }
+    }
 
-        return contactlist;
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                CONTACT_PROJECTION, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mAdapter.swapCursor(data);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
 
     }
 }
